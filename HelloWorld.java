ea75d929007a0149f1fba7335dbece3a7388fd0f
danielpohl12345 <142402215+danielpohl12345@users.noreply.github.com>
2024-04-16 14:20:32 +0200

    Add main method

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("Java is cool!");
+
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println(sum);
+
+          int
+        float Step 2: Compare 2 'int' values for: greater than, less than, equality, and print the results on the console
+        double Step 3: Repeat steps 1 and 2, but this time use 'float' and 'double' values.
+
+        */
+        int a = 5;
+        int b = 7;
+        float c = 5.14f;
+        float d = 9.84f;
+        double e = 4.5;
+        double f = 2.7;
+        System.out.println(d - c);
+        System.out.println(e - f);
+
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+
+        System.out.println(c > d);
+        System.out.println(c < d);
+        System.out.println(c == d);
+
+        System.out.println(e > f);
+        System.out.println(e < f);
+        System.out.println(e == f);
+
+    }
+
+}
