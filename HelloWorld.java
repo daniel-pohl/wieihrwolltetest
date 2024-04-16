af66506921c88921671d95ecde21a2c60f94be06
danielpohl12345 <142402215+danielpohl12345@users.noreply.github.com>
2024-04-16 14:39:36 +0200

    version3

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         int version2git;
+        int version3git;
+
         /*
         System.out.println("Java is cool!");
 
